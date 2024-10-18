5b7faae56dd073e8c7a91be8ad2bb893460c8818
saraegio <saraegio23@gmail.com>
2024-10-18 17:12:05 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
+import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
+
 export const metadata = {
   title: 'Sara & Gio',
   description: 'Fino alla fine del mondo',
@@ -11,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="it">
-      <body>{children}</body>
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
